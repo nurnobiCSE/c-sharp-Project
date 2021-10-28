772d8f311a6712d13e442de5f9f37d2622cdb870
nurnobiCSE <shuvo517942@gmail.com>
2021-10-28 20:23:29 +0600

    add export pdf tool v-1

--- test/viewmembers.Designer.cs
+++ test/viewmembers.Designer.cs
@@ -29,17 +29,23 @@ namespace test
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.closeAdd = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.viewGridView = new System.Windows.Forms.DataGridView();
+            this.zymDbDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.zymDbDataSet = new test.zymDbDataSet();
             this.searchview = new System.Windows.Forms.Button();
             this.search = new System.Windows.Forms.RichTextBox();
             this.backview = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.clears = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.viewGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zymDbDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zymDbDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // closeAdd
@@ -83,6 +89,8 @@ namespace test
             // 
             // viewGridView
             // 
+            this.viewGridView.AllowUserToAddRows = false;
+            this.viewGridView.AllowUserToOrderColumns = true;
             this.viewGridView.BackgroundColor = System.Drawing.SystemColors.Info;
             this.viewGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.viewGridView.Location = new System.Drawing.Point(108, 167);
@@ -99,6 +107,16 @@ namespace test
             this.viewGridView.Size = new System.Drawing.Size(847, 326);
             this.viewGridView.TabIndex = 27;
             this.viewGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.viewGridView_CellContentClick);
+            // 
+            // zymDbDataSetBindingSource
+            // 
+            this.zymDbDataSetBindingSource.DataSource = this.zymDbDataSet;
+            this.zymDbDataSetBindingSource.Position = 0;
+            // 
+            // zymDbDataSet
+            // 
+            this.zymDbDataSet.DataSetName = "zymDbDataSet";
+            this.zymDbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // searchview
             // 
@@ -165,11 +183,26 @@ namespace test
             this.clears.UseVisualStyleBackColor = false;
             this.clears.Click += new System.EventHandler(this.clears_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(839, 494);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 30);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Export to PDF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // viewmembers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1079, 528);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.clears);
             this.Controls.Add(this.backview);
             this.Controls.Add(this.searchview);
@@ -185,6 +218,8 @@ namespace test
             this.Text = "viewmembers";
             this.Load += new System.EventHandler(this.viewmembers_Load);
             ((System.ComponentModel.ISupportInitialize)(this.viewGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zymDbDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zymDbDataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,5 +236,8 @@ namespace test
         private System.Windows.Forms.Button backview;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button clears;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource zymDbDataSetBindingSource;
+        private zymDbDataSet zymDbDataSet;
     }
 }
--- test/viewmembers.cs
+++ test/viewmembers.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -100,6 +103,79 @@ namespace test
             search.Text = "";
             viewmembers refresh = new viewmembers();
             refresh.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(viewGridView.Rows.Count > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF (*.pdf)|*.pdf";
+                sfd.FileName = "Output.pdf";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+                            PdfPTable pdfTable = new PdfPTable(viewGridView.Columns.Count);
+                            pdfTable.DefaultCell.Padding = 3;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                            foreach (DataGridViewColumn column in viewGridView.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                                pdfTable.AddCell(cell);
+                               
+                            }
+                            
+                            foreach (DataGridViewRow row in viewGridView.Rows)
+                            {
+                                foreach (DataGridViewCell cell in row.Cells)
+                                {
+                                    pdfTable.AddCell(cell.Value.ToString());
+                                }
+                            }
+
+                            using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
+                            {
+                                Document pdfDoc = new Document(PageSize.A4, 10f, 20f, 20f, 10f);
+                                PdfWriter.GetInstance(pdfDoc, stream);
+                                pdfDoc.Open();
+                                pdfDoc.Add(pdfTable);
+                                pdfDoc.Close();
+                                stream.Close();
+                            }
+
+                            MessageBox.Show("Data Exported Successfully !!!", "Info");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Record To Export !!!", "Info");
+            }
+        
+                 
         }
     }
 }
